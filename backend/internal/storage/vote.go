19835fb650be4788be1e3b825057e5066c6f8073
onee-only <kimww0306@gmail.com>
2024-11-06 11:04:38 +0900

    Add vote storage and its memory implementation

---
+++ backend/internal/storage/vote.go
@@ -0,0 +1,19 @@
+package storage
+
+import (
+	"context"
+
+	"github.com/onee-only/typewriter/backend/internal/object"
+)
+
+// VoteStorage keeps the vote counts of cursor's next direction.
+// Operations should be atomic since multiple concurrent entities can access simultaneously.
+type VoteStorage interface {
+	// Vote increases counter of given direction by 1.
+	Vote(ctx context.Context, dir object.Direction) error
+	// Status returns current vote count of all directions.
+	// The order of directions will be order of [object.Direction].
+	Status(ctx context.Context) (votes [4]uint, err error)
+	// Reset resets vote count of directions.
+	Reset(ctx context.Context) error
+}
